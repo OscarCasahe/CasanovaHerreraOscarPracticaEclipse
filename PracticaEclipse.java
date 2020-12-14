d18a828d02afef2f7fb09884895b3fc710c513eb
Óscar Casanova <71701602+OscarCasahe@users.noreply.github.com>
2020-12-14 12:59:15 +0100

    Update PracticaEclipse.java

--- PracticaEclipse.java
+++ PracticaEclipse.java
@@ -5,7 +5,8 @@ public class PracticaEclipse {
     public static void main(String args[])
     {
         System.out.println("Hola Mundo");
-        
+        System.out.println("Hola Mundo desde GitHub");
+		
     }
 
 }
